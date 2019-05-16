096454b9f0427815d5d793a0255d341c5e34d65f
Aleksandr Verkhovskii <verhoffsky@mail.ru>
2019-05-16 08:45:31 +0300

    add constant to resource .xml correct handler for the pressing of the button "Clear"

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -12,8 +11,6 @@ public class MainActivity extends AppCompatActivity {
     private EditText editTextName;
     private EditText editTextMail;
     private TextView successfulSubscribe;
-    private Button okButton;
-    private Button deleteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         findViewById(R.id.button_ok).setOnClickListener(okClickListener);
         findViewById(R.id.button_delete).setOnClickListener(deleteClickListener);
-
     }
-
     private View.OnClickListener okClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -39,13 +34,12 @@ public class MainActivity extends AppCompatActivity {
             successfulSubscribe.setText(message);
         }
     };
-
     private View.OnClickListener deleteClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            editTextName.setText(null);
-            editTextMail.setText(null);
-            successfulSubscribe.setText(null);
+            editTextName.getText().clear();
+            editTextMail.getText().clear();
+            successfulSubscribe.setText("");
             }
     };
 }
